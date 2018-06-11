e07e57b0860e3d19486d3d5b97a2acd8534cc3a8
Ashish <ashish.mishra41191@gmail.com>
2018-06-11 21:13:33 +0700

    base structure

---
+++ src/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Zone from '../containers/zone';
+
+class App extends Component {
+  render() {
+    return (
+      <Zone />
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/zone/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Zone extends React.Component {
+  render() {
+    return (<div>ZONE</div>);
+  }
+}
+
+export default Zone;
